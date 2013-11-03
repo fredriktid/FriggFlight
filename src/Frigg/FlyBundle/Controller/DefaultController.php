2b99dacae41da62e9384416aec028cc64190ff83
Fredrik Tidemann <fredrik@netmaking.no>
2013-11-03 14:52:42 +0100

    Basic entities

--- src/Frigg/FlyBundle/Controller/DefaultController.php
+++ src/Frigg/FlyBundle/Controller/DefaultController.php
@@ -3,14 +3,18 @@
 namespace Frigg\FlyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="frigg_fly_homepage")
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('FriggFlyBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('FriggFlyBundle:Default:index.html.twig');
     }
 }
---
+++ src/Frigg/FlyBundle/Entity/Airport.php
@@ -0,0 +1,35 @@
+<?php
+use Doctrine\ORM\Mapping AS ORM;
+
+/** 
+ * @ORM\Entity
+ */
+class Airport
+{
+    /** 
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /** 
+     * @ORM\Column(type="string", unique=true, length=20, nullable=true)
+     */
+    private $code;
+
+    /** 
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /** 
+     * @ORM\OneToMany(targetEntity="Flight", mappedBy="airport")
+     */
+    private $flights;
+
+    /** 
+     * @ORM\ManyToMany(targetEntity="Flight", mappedBy="via_airports")
+     */
+    private $via_flights;
+}
