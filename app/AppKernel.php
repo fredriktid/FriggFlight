d2c6ea128bb623367b3321fc5a1dbf297f337e89
fredriktid <fredrik.tid@gmail.com>
2013-11-11 18:52:54 +0100

    Installed FOSOAuthServerBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,11 +21,11 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle($this),
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
             new Frigg\FlightBundle\FriggFlightBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
